162e2fab494059e967d94892ac5a62365437c34e
Nonamechick <otajonovxasan350@gmail.com>
2025-01-20 11:01:21 +0530

    go to top button added

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,3 +207,20 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+// JavaScript to control the "Go to Top" button behavior
+const goToTopBtn = document.getElementById("goToTopBtn");
+
+// Show the button when scrolled down
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goToTopBtn.style.display = "block";
+  } else {
+    goToTopBtn.style.display = "none";
+  }
+};
+
+// Scroll to the top when the button is clicked
+goToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
